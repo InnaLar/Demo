c4962b9d8c37673555d7ca890342e639c31f9a54
Инна Ларина <innalar@mail.ru>
2024-03-10 18:35:09 +0500

    homework 10.03.2024

--- src/test/java/com/example/demo/spring/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/spring/controller/UserControllerTest.java
@@ -11,8 +11,8 @@ import com.example.demo.spring.model.dto.UserShortRq;
 import com.example.demo.spring.model.entity.Doc;
 import com.example.demo.spring.model.entity.User;
 import com.example.demo.spring.support.IntegrationTestBase;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.List;
 
@@ -22,8 +22,34 @@ class UserControllerTest extends IntegrationTestBase {
 
     @Test
     void getUserList() {
-        assertThat(userController.getUserList())
-            .isEmpty();
+        //GIVEN
+        final User user = User.builder()
+            .email("InnaLarina021@gmail.ru")
+            .password("123")
+            .build()
+            .withDoc(Doc.builder()
+                .title("schema1")
+                .build());
+        userRepository.save(user);
+
+        //WHEN
+        final WebTestClient.ResponseSpec response = webTestClient.get()
+            .uri("/api/v1/users")
+            .exchange();
+
+        //THEN
+        response
+            .expectStatus()
+            .isOk()
+            .expectBodyList(UserRs.class)
+            .isEqualTo(List.of(UserRs.builder()
+                .id(1L)
+                .email("InnaLarina021@gmail.ru")
+                .docRs(List.of(DocRs.builder()
+                    .id(1L)
+                    .title("schema1")
+                    .userId(1L)
+                    .build())).build()));
     }
 
     @Test
@@ -36,12 +62,16 @@ class UserControllerTest extends IntegrationTestBase {
             .withDoc(Doc.builder().title("document1").build());
         userRepository.save(user);
         //WHEN
-        final UserRs userRs = transactionTemplate
-            .execute(ts -> userController.getUserById(1L));
-        //THEN
-        assertThat(userRs)
-            .usingRecursiveComparison()
-            .ignoringFields("id", "docList.id", "docList.userId")
+        final WebTestClient.ResponseSpec response = webTestClient.get()
+            .uri(uriBuilder -> uriBuilder
+                .pathSegment("api", "v1", "users", "1")
+                .build())
+            .exchange();
+
+        response
+            .expectStatus()
+            .isOk()
+            .expectBody(UserRs.class)
             .isEqualTo(UserRs.builder()
                 .id(1L)
                 .email("InnaLarina021@jmail.ru")
@@ -64,17 +94,35 @@ class UserControllerTest extends IntegrationTestBase {
         userRepository.save(user);
 
         //WHEN
-        userController.deleteUserById(1L);
+        final WebTestClient.ResponseSpec response = webTestClient
+            .delete()
+            .uri(uriBuilder -> uriBuilder
+                .path("api/v1/users/{id}")
+                .build(1L))
+            .exchange();
         //THEN
+        response
+            .expectStatus()
+            .isOk();
+
         assertThat(userRepository.findAll())
             .isEmpty();
     }
 
     @Test
     void shouldThrowExceptionDeleteByWrongId() {
-        Assertions.assertThatThrownBy(() -> userController.deleteUserById(5L))
+        /*Assertions.assertThatThrownBy(() -> userController.deleteUserById(5L))
             .isInstanceOf(ServiceException.class)
-            .hasMessage("User with id %s not found", 5L);
+            .hasMessage("User with id %s not found", 5L);*/
+        webTestClient
+            .delete()
+            .uri(uriBuilder -> uriBuilder
+                .pathSegment("api", "v1", "users", "5")
+                .build())
+            .exchange()
+            .expectStatus()
+            .isEqualTo(404);
+
     }
 
     @Test
@@ -86,14 +134,20 @@ class UserControllerTest extends IntegrationTestBase {
             .build();
 
         //WHEN
-        final UserRs userRs = userController.postUser(request);
+        final WebTestClient.ResponseSpec response = webTestClient
+            .post()
+            .uri("api/v1/users/post1")
+            .bodyValue(request)
+            .exchange();
 
         //THEN
-        assertThat(userRs)
-            .usingRecursiveComparison()
-            .ignoringFields("id")
+        response
+            .expectStatus()
+            .isOk()
+            .expectBody(UserRs.class)
             .isEqualTo(UserRs.builder()
                 .email("example@mail.ru")
+                .id(1L)
                 .docRs(List.of())
                 .build());
 
@@ -107,7 +161,6 @@ class UserControllerTest extends IntegrationTestBase {
                     .password("321")
                     .build()
                 ));
-
     }
 
     @Test
@@ -124,20 +177,25 @@ class UserControllerTest extends IntegrationTestBase {
             )).build();
 
         //WHEN
-        final UserRs userRs = userController.postUserListDoc(request);
+        final WebTestClient.ResponseSpec response = webTestClient
+            .post()
+            .uri("api/v1/users/post2")
+            .bodyValue(request)
+            .exchange();
 
-        //THEN
-        assertThat(userRs)
-            .usingRecursiveComparison()
-            .ignoringFields("id")
+        response.expectStatus()
+            .isOk()
+            .expectBody(UserRs.class)
             .isEqualTo(UserRs.builder()
                 .email("InnaLar@mail.ru")
+                .id(1L)
                 .docRs(List.of(DocRs.builder()
                     .id(1L)
                     .title("document1")
                     .userId(1L)
                     .build()))
                 .build());
+
         transactionTemplate.execute(
             ts -> assertThat(userRepository.findById(1L)
                 .orElseThrow(() -> new ServiceException(ErrorCode.ERR_CODE_001, 1L)))
@@ -170,14 +228,22 @@ class UserControllerTest extends IntegrationTestBase {
             .email("colobocLSR@mail.ru")
             .id(1L)
             .build();
-        final UserRs userRs = userController.putUser(userRsToPut);
+        //final UserRs userRs = userController.putUser(userRsToPut);
+        final WebTestClient.ResponseSpec response = webTestClient
+            .put()
+            .uri("api/v1/users")
+            .bodyValue(userRsToPut)
+            .exchange();
 
         //THEN
-        assertThat(userRs)
+        response
+            .expectStatus()
+            .isOk()
+            .expectBody(UserRs.class)
             .isEqualTo(UserRs.builder()
-                .docRs(List.of())
                 .id(1L)
                 .email("colobocLSR@mail.ru")
+                .docRs(List.of())
                 .build());
 
         transactionTemplate.execute(ts -> assertThat(userRepository.findById(1L)
@@ -189,5 +255,4 @@ class UserControllerTest extends IntegrationTestBase {
                 .password("321")
                 .build()));
     }
-
 }
